af2a509749dda605c4d18f8127a5f624814fcf50
cvairlis <cvairlis@gmail.com>
2022-04-17 15:48:50 +0300

    start with testing the happy path

--- tests/Unit/FizzBuzz/FizzBuzzTestCase.php
+++ tests/Unit/FizzBuzz/FizzBuzzTestCase.php
@@ -13,8 +13,6 @@ class FizzBuzzTestCase extends UnitTestCase
         return new FizzBuzz();
     }
 
-    /**
-     */
     public function throws_exception_data_provider():array
     {
         return [
@@ -30,9 +28,31 @@ class FizzBuzzTestCase extends UnitTestCase
      *
      * @dataProvider throws_exception_data_provider
      */
-    public function it_throws_exception_when_stop_is_less_than_start(int $start, int $stop):void
+    public function it_throws_exception_for_invalid_values_on_start_stop(int $start, int $stop):void
     {
         $this->expectException(InvalidArgumentException::class);
         $this->makeFizzBuzz()->fizzBuzz($start, $stop);
+    }
+
+    public function success_calculation_data_provider():array
+    {
+        return [
+            '1' => [1, 1, '1'],
+            '2' => [2, 2, '2'],
+            'Fizz3' => [3, 3, 'Fizz'],
+            'Fizz6' => [6, 6, 'Fizz'],
+            'Fizz9' => [9, 9, 'Fizz'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider success_calculation_data_provider
+     */
+    public function it_returns_correct_string_sequence(int $start, int $stop, string $expected):void
+    {
+        $fizz_buzz = $this->makeFizzBuzz()->fizzBuzz($start, $stop);
+        $this->assertEquals($fizz_buzz, $expected);
     }
 }
