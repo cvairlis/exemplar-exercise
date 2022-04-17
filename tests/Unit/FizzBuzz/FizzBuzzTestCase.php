bd8f7691136ee840ab3ca4019044d746f0aaa21e
cvairlis <cvairlis@gmail.com>
2022-04-17 14:37:18 +0300

    improve with data provider

--- tests/Unit/FizzBuzz/FizzBuzzTestCase.php
+++ tests/Unit/FizzBuzz/FizzBuzzTestCase.php
@@ -14,11 +14,25 @@ class FizzBuzzTestCase extends UnitTestCase
     }
 
     /**
-     * @test
      */
-    public function it_throws_exception_when_stop_is_less_than_start():void
+    public function throws_exception_data_provider():array
+    {
+        return [
+            '$stop < $start' => [100, 99],
+            '$start < 0' => [-10, 10],
+            '$stop < 0' => [10, -10]
+
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider throws_exception_data_provider
+     */
+    public function it_throws_exception_when_stop_is_less_than_start(int $start, int $stop):void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->makeFizzBuzz()->fizzBuzz(1, 0);
+        $this->makeFizzBuzz()->fizzBuzz($start, $stop);
     }
 }
